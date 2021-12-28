4caa8d32193e267462e6ced92ca29a1d928242c4
Dan Michael O. Heggø <danmichaelo@gmail.com>
2021-12-28 12:20:39 +0100

    Add minimal schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,16 +1,32 @@
-// First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
-
-// Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-// Then we give our schema to the builder and provide the result to Sanity
+const building = {
+  name: "building",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "string"
+    }
+  ]
+}
+
+const room = {
+  name: "room",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "string"
+    }
+  ]
+}
+
 export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
+  name: 'default-model',
   types: schemaTypes.concat([
-    /* Your types here! */
+    building,
+    room,
   ]),
 })
